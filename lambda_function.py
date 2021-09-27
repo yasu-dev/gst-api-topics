181a33e7455483ef513fd34613edc2edafc4dc35
EC2 Default User <ec2-user@ip-172-31-32-184.ap-northeast-1.compute.internal>
2021-09-27 06:45:25 +0000

    update

--- lambda_function.py
+++ lambda_function.py
@@ -11,5 +11,5 @@ table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
 
-    logger.info("GET /api/v1/topics")
+    logger.info("get_topics_information")
     return table.scan()
