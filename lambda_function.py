d9ab48edf72f47c1636f1647240ebada9a027cf9
yasu-dev <cloudshell-user@ip-10-0-92-217.ap-northeast-1.compute.internal>
2021-09-27 05:32:58 +0000

    update

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import logging
+import boto3
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'Topic'
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event, context):
+
+    logger.info("GET /api/v1/topics")
+    return table.scan()
