399d0fcdcd37fa7e6dfa22f482421423ea23c70e
EC2 Default User <ec2-user@ip-172-31-32-184.ap-northeast-1.compute.internal>
2021-09-28 01:25:18 +0000

    update

--- index.py
+++ index.py
@@ -11,5 +11,13 @@ table = dynamodb.Table(table_name)
 
 def handler(event, context):
 
-    logger.info("get_topics_information")
-    return table.scan()
+    logger.info('get_topics_information')
+
+    response = table.scan()
+    data = response['Items']
+
+    return {
+            'headers': {'Content-Type': 'application/json'},
+            'statusCode': 200,
+            'body': json.dumps(data)
+            }
